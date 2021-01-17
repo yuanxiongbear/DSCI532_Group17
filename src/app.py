b87d8f90778b010013144af99818245c9307c235
Marco Ma <38771197+mmyz88@users.noreply.github.com>
2021-01-16 20:49:25 -0800

    Improve overall writings and add starter code (#4)
    * Update README.md to make it more readable
    
    * Update README.md to make it more readable
    
    * Update README.md to make it more readable
    
    * Update Proposal.md to for grammatical issues
    
    * Add data_manager.py, add IFrame

--- src/app.py
+++ src/app.py
@@ -1,10 +1,24 @@
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
+from data_manager import DataManager
 
-app = dash.Dash()
+# retrieve data
+data = DataManager().get_data()
+table = data[['Ranking', 'Name', 'Nationality', 'Age', 'Value', 'Overall']]
+table = table.sort_values('Overall', ascending=False)[:10]
+
+app = dash.Dash(__name__)
+
+# app layout
 app.layout = html.Div([
-    html.H1('FIFA Star Board')
+    html.Center(
+        html.H1('FIFA Star Board')
+    ),
+    html.Iframe(
+        srcDoc=table.to_html(index=False),
+        style={'border-width': '0', 'width': '50%', 'height': '400px'}
+    )
 ])
 
 if __name__ == '__main__':
