21d16befe17871f27d0ea8218e5633b453c16503
mmyz <yzmarco.ma@gmail.com>
2021-01-19 17:03:07 -0800

    Add sort-by and order dropdowns and link to table

--- src/app.py
+++ src/app.py
@@ -1,25 +1,59 @@
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
+from dash.dependencies import Input, Output
 from data_manager import DataManager
+import altair as alt
+import dash_bootstrap_components as dbc
 
 # retrieve data
 data = DataManager().get_data()
-table = data[['Ranking', 'Name', 'Nationality', 'Age', 'Value', 'Overall']]
-table = table.sort_values('Overall', ascending=False)[:10]
 
-app = dash.Dash(__name__)
+
+table = DataManager().make_table(data)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # app layout
-app.layout = html.Div([
-    html.Center(
-        html.H1('FIFA Star Board')
-    ),
-    html.Iframe(
-        srcDoc=table.to_html(index=False),
-        style={'border-width': '0', 'width': '50%', 'height': '400px'}
-    )
+app.layout = dbc.Container([
+    html.H1('FIFA Star Board'), 
+    html.Br(),
+    dbc.Row([
+        dbc.Col([
+            dcc.Dropdown(
+                id='sortby-widget',
+                value='Overall',
+                options=[{'label': col, 'value': col} for col in data.columns]
+            ), 
+            html.Br(),
+            dcc.Dropdown(
+                id='order-widget',
+                value='True',
+                options=[{'label': 'Descending', 'value': 'False'},
+                         {'label': 'Ascending', 'value': 'True'}]
+            )
+        ], md=3),
+        dbc.Col([
+            html.Iframe(
+                id='table',
+                srcDoc=table.to_html(index=False),
+                style={'border-width': '0', 'width': '100%', 'height': '400px'}
+            ),
+        ]),
+        dbc.Col([
+            html.Div(['Right section'],
+            style={"border":"2px black solid"})
+        ], md=3)
+    ])
 ])
+
+@app.callback(
+    Output('table', 'srcDoc'),
+    Input('sortby-widget', 'value'),
+    Input('order-widget', 'value'))
+def update_table(by, order):
+    table = DataManager().update_table(data, by, order=='True')
+    return table.to_html(index=False)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- src/data_manager.py
+++ src/data_manager.py
@@ -9,5 +9,18 @@ class DataManager():
 
     def get_data(self):
         df = pd.read_csv('../data/data.csv')
-        df['Ranking'] = np.arange(df.shape[0]) + 1
         return df
+
+    def make_table(self, df):
+        table = df[['Name', 'Nationality', 'Age', 'Value', 'Overall']]
+        table = table.sort_values('Overall', ascending=False)[:10]
+        table['Ranking'] = np.arange(table.shape[0]) + 1
+        return table
+
+    def update_table(self, df, by, order):
+        table = df[['Name', 'Nationality', 'Age', 'Value', 'Overall']]
+        table = table.sort_values(by=by, ascending=order)[:10]
+        table['Ranking'] = np.arange(table.shape[0]) + 1
+        return table
+
+
