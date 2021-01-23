8eb66a1ab0be1c25b71af77a499397cb8e8795d7
Guanshu Tao <guanshu@ualberta.ca>
2021-01-23 10:59:03 -0700

    add continent column and update layout

--- src/app.py
+++ src/app.py
@@ -8,18 +8,88 @@ data = DataManager().get_data()
 table = data[['Ranking', 'Name', 'Nationality', 'Age', 'Value', 'Overall']]
 table = table.sort_values('Overall', ascending=False)[:10]
 
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # app layout
-app.layout = html.Div([
-    html.Center(
-        html.H1('FIFA Star Board')
-    ),
-    html.Iframe(
-        srcDoc=table.to_html(index=False),
-        style={'border-width': '0', 'width': '50%', 'height': '400px'}
-    )
+app.layout = dbc.Container([
+    html.H1('FIFA Star Board'),
+    html.Br(),
+    dbc.Row([
+        dbc.Col([
+            html.Div(['Rank By:']),
+            dcc.Dropdown(
+                id='sortby-widget',
+                value='Overall',
+                options=[{'label': col, 'value': col} for col in data.columns]
+            ), 
+            html.Br(),
+            html.Div(['Order:']),
+            dcc.Dropdown(
+                id='order-widget',
+                value='True',
+                options=[{'label': 'Descending', 'value': 'False'},
+                         {'label': 'Ascending', 'value': 'True'}]
+            ),
+            html.Br(),
+            html.Div(['Nationality:']),
+            dcc.Dropdown(
+                id='filter-natn-widget',
+                value='',
+                options=[{'label': val, 'value': val} for val in data['Nationality'].unique()]
+            ),
+            html.Br(),
+            html.Div(['Club:']),
+            dcc.Dropdown(
+                id='filter-club-widget',
+                value='',
+                options=[{'label': val, 'value': val} for val in data['Club'].dropna().unique()]
+            ),
+            html.Br(),
+            html.Div(['Continent:']),
+            dcc.Dropdown(
+                id='filter-conti-widget',
+                value='',
+                options=[{'label': val, 'value': val} for val in data['Continent'].unique()
+            
+        ], md=3),
+        dbc.Col([
+            html.H4(['Select Attributes:']),
+            dcc.Dropdown(
+                id='attribute-widget',
+                value=['Name', 'Nationality', 'Age', 'Value', 'Overall'],
+                options=[{'label': col, 'value': col} for col in data.columns],
+                multi=True
+            ),
+            html.Iframe(
+                id='table',
+                srcDoc=table.to_html(index=False),
+                style={'border-width': '0', 'width': '100%', 'height': '400px'}
+            ),
+        ]),
+        dbc.Col([
+            html.Iframe(
+                id='bycountry-chart',
+                srcDoc=chart_top.to_html(),
+                style={'border-width': '0', 'width': '150%', 'height': '300px'}
+            ),
+            html.Iframe(
+                id='byclub-chart',
+                srcDoc=chart_bot.to_html(),
+                style={'border-width': '0', 'width': '150%', 'height': '300px'}
+            ),
+        ], md=3)
+    ])
 ])
+
+
+@app.callback(
+    Output('table', 'srcDoc'),
+    Input('sortby-widget', 'value'),
+    Input('order-widget', 'value'),
+    Input('attribute-widget', 'value'),
+    Input('filter-natn-widget', 'value'),
+    Input('filter-club-widget', 'value'),
+    Input('filter-conti-widget', 'value'))
 
 if __name__ == '__main__':
     app.run_server(debug=True)
